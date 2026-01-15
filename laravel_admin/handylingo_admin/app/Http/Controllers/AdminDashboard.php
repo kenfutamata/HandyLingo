6c8d80e4e5616cfe837318b1a5a1ca1b5e1041fc
kenfutamata <kenfutamata123@gmail.com>
2026-01-16 06:50:43 +0800

    Dashboard updated

--- laravel_admin/handylingo_admin/app/Http/Controllers/AdminDashboard.php
+++ laravel_admin/handylingo_admin/app/Http/Controllers/AdminDashboard.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Feedbacks;
 use App\Models\Users;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -10,14 +11,15 @@ class AdminDashboard extends Controller
 {
     public function index(){
         $user = Auth::guard('admin')->user();
+        $feedbackCount = Feedbacks::where('status', 'New')->count();
         $userCount = Users::where('role', 'user')->where('status', 'active')->count(); 
-        return view('admin.admin_dashboard', compact('user', 'userCount')); 
+        return view('admin.admin_dashboard', compact('user', 'userCount', 'feedbackCount')); 
     }
 
     public function logout(Request $request){
     Auth::guard('admin')->logout();
         $request->session()->invalidate();
         $request->session()->regenerateToken();
-        return redirect()->route('admin.login');
+        return redirect()->route('admin.landingpage');
     }
 }
