f802375d715137dd656ff6d42a9e7cb952b4ec3f
kenfutamata <kenfutamata123@gmail.com>
2026-04-23 12:00:12 +0800

    updated user table migration and landing page

--- laravel_admin/handylingo_admin/resources/views/admin/landing_page.blade.php
+++ laravel_admin/handylingo_admin/resources/views/admin/landing_page.blade.php
@@ -82,7 +82,7 @@
 
                 <!-- CTA Buttons -->
                 <div class="flex flex-col sm:flex-row gap-4 justify-center lg:justify-start">
-                    <a href="https://drive.google.com/drive/folders/1GrssiYSEiHRrXaHJFnBJFLeRNB91Nf2c?usp=sharing"
+                    <a href="https://www.dropbox.com/scl/fi/0x7c2jpm1cbs8rc2luwsz/handylingo.apk?rlkey=7yx2mjbgr6gz17xde049qlksn&st=lt3rlz99&dl=1"
                         class="inline-flex items-center justify-center gap-2 bg-green-500 hover:bg-green-600 text-white text-lg font-semibold px-8 py-3 rounded-xl shadow-lg transition duration-300">
                         Download App
                     </a>
